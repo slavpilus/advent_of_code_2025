917f1c4d61e089bc052e023f48c9501ac516c5a8
Slav Pilus <slav@pilus.me.uk>
2025-12-01 06:13:02 +0000

    day 1 part 1

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,8 +1,47 @@
 defmodule AOC.Day01 do
   def part1(input) do
-    IO.puts(input)
-    IO.puts("part 1")
-    "part 1"
+    {_, counter} =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line -> extract(line) end)
+      |> Enum.reduce({50, 0}, fn {rotation, {moves}}, {position, counter} ->
+        case rotation do
+          "R" ->
+            if position + moves > 99 do
+              reminder = rem(position + moves, 100)
+
+              if reminder == 0 do
+                {reminder, counter + 1}
+              else
+                {reminder, counter}
+              end
+            else
+              {position + moves, counter}
+            end
+
+          "L" ->
+            if position - moves < 1 do
+              reminder = abs(rem(position - moves, 100))
+
+              if reminder == 0 do
+                {reminder, counter + 1}
+              else
+                {100 - reminder, counter}
+              end
+            else
+              {position - moves, counter}
+            end
+        end
+      end)
+
+    counter
+  end
+
+  defp extract(line) do
+    case Regex.run(~r/(L|R)(\d+)/, line) do
+      [_, dir, step] ->
+        {dir, {String.to_integer(step)}}
+    end
   end
 
   def part2(_input) do
--- test/day_01_test.exs
+++ test/day_01_test.exs
@@ -2,6 +2,20 @@ defmodule AOC.Day01Test do
   use ExUnit.Case
 
   test "day01part1" do
-    assert AOC.Day01.part1("blah") == "part 1"
+    assert AOC.Day01.part1("L5") == 0
+    assert AOC.Day01.part1("L50") == 1
+    assert AOC.Day01.part1("L150") == 1
+    assert AOC.Day01.part1("R50") == 1
+    assert AOC.Day01.part1("R5") == 0
+    assert AOC.Day01.part1("L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82") == 3
   end
 end
