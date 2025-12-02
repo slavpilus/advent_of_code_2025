ad4d902d66f674acf9c69f2c13d129469750a37d
Slav Pilus <slav@pilus.me.uk>
2025-12-02 17:30:19 +0000

    day 2 part 1

---
+++ lib/day02.ex
@@ -0,0 +1,35 @@
+defmodule AOC.Day02 do
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.flat_map(fn line -> String.split(line, ",") end)
+    |> Enum.map(fn range -> get_range(range) end)
+    |> Enum.map(fn range -> find_incorrect(range) end)
+    |> Enum.sum()
+  end
+
+  defp find_incorrect({start, stop}) do
+    start..stop
+    |> Enum.map(fn n -> Integer.to_string(n) end)
+    |> Enum.reduce(0, fn v, i ->
+      {l, r} = String.split_at(v, div(String.length(v), 2))
+
+      if l == r do
+        i + String.to_integer(v)
+      else
+        i
+      end
+    end)
+  end
+
+  defp get_range(text) do
+    case Regex.run(~r/(\d+)-(\d+)/, text) do
+      [_, start, stop] ->
+        {String.to_integer(start), String.to_integer(stop)}
+    end
+  end
+
+  def part2(input) do
+    IO.inspect(input)
+  end
+end
