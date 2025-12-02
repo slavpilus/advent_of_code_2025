15e589d36b0020a3fe024d0c36cfa1185a7acc90
Slav Pilus <slav@pilus.me.uk>
2025-12-02 16:09:11 +0000

    day 1 part 2

--- lib/day01.ex
+++ lib/day01.ex
@@ -44,8 +44,48 @@ defmodule AOC.Day01 do
     end
   end
 
-  def part2(_input) do
-    IO.puts("part 2")
-    "part 2"
+  def part2(input) do
+    {_, counter} =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line -> extract(line) end)
+      |> Enum.reduce({50, 0}, fn {rotation, {moves}}, {position, counter} ->
+        case rotation do
+          "R" ->
+            if position + moves > 99 do
+              whole = div(position + moves, 100)
+              reminder = rem(position + moves, 100)
+
+              cond do
+                whole != 0 and reminder != 0 -> {reminder, counter + whole}
+                whole != 0 -> {reminder, counter + whole}
+                true -> {100 - reminder, counter}
+              end
+            else
+              {position + moves, counter}
+            end
+
+          "L" ->
+            if position - moves < 1 do
+              reminder = rem(moves, 100)
+              whole = div(moves, 100)
+              new_pos = Integer.mod(position - moves + 100, 100)
+
+              extra =
+                if position != 0 and reminder > 0 and position - reminder <= 0 do
+                  1
+                else
+                  0
+                end
+
+              all_crosses = whole + extra
+              {new_pos, counter + all_crosses}
+            else
+              {position - moves, counter}
+            end
+        end
+      end)
+
+    counter
   end
 end
--- test/day_01_test.exs
+++ test/day_01_test.exs
@@ -18,4 +18,22 @@ L99
 R14
 L82") == 3
   end
+
+  test "day01part2" do
+    assert AOC.Day01.part2("L50\nL10") == 1
+    assert AOC.Day01.part2("R50\nR100") == 2
+    assert AOC.Day01.part2("L50\nL100") == 2
+    assert AOC.Day01.part2("L50\nL10") == 1
+    assert AOC.Day01.part2("R50\nR100") == 2
+    assert AOC.Day01.part2("L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82") == 6
+  end
 end
