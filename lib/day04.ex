33ef434e571d38be190f145e3d12dc4d2296373e
Slav Pilus <slav@pilus.me.uk>
2025-12-05 20:11:56 +0000

    day 4 part 2

--- lib/day04.ex
+++ lib/day04.ex
@@ -44,6 +44,49 @@ defmodule AOC.Day04 do
   end
 
   def part2(input) do
-    IO.puts(input)
+    grid =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {row, r} ->
+        row
+        |> String.codepoints()
+        |> Enum.with_index()
+        |> Enum.map(fn {char, c} -> {{r, c}, char} end)
+      end)
+      |> Map.new()
+
+    process_until_done(grid, 0)
+  end
+
+  defp process_until_done(grid, total) do
+    {new_grid, count} = process_grid(grid)
+
+    if count == 0 do
+      total
+    else
+      process_until_done(new_grid, total + count)
+    end
+  end
+
+  defp process_grid(grid) do
+    {new_grid, counter} =
+      Enum.reduce(grid, {Map.new(), 0}, fn {{x, y}, cell}, {acc_grid, acc_count} ->
+        if cell != "@" do
+          {Map.put(acc_grid, {x, y}, "."), acc_count}
+        else
+          neighbor_count =
+            neighbors({x, y})
+            |> Enum.count(fn coord -> Map.get(grid, coord) == "@" end)
+
+          if neighbor_count < 4 do
+            {Map.put(acc_grid, {x, y}, "."), acc_count + 1}
+          else
+            {Map.put(acc_grid, {x, y}, "@"), acc_count}
+          end
+        end
+      end)
+
+    {new_grid, counter}
   end
 end
